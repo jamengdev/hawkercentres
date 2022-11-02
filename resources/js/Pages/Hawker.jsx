4908e354da5482037c3b4932f61a1e623a9a7ad1
Jame Ng <build@jameng.dev>
2022-11-02 11:38:54 +0800

    style hawker detail page

--- resources/js/Pages/Hawker.jsx
+++ resources/js/Pages/Hawker.jsx
@@ -1,27 +1,53 @@
-import { Head } from "@inertiajs/inertia-react";
+import { Head, Link } from "@inertiajs/inertia-react";
 
 export default function Welcome({ hawker }) {
     const data = JSON.parse(hawker.api_data);
-
+    console.log(data);
     return (
         <>
             <Head title={data.name} />
-            <div className="h-screen px-4 mt-24 lg:mt-0 lg:flex lg:flex-col lg:items-center lg:justify-center">
-                <div className="text-5xl text-center mb-4">{data.name}</div>
-                <div className="text-3xl text-center mb-2">
-                    Scheduled Cleaning
-                </div>
-                <div>
-                    {data.q1_cleaningenddate} to {data.q1_cleaningstartdate}
-                </div>
-                <div>
-                    {data.q2_cleaningenddate} to {data.q2_cleaningstartdate}
-                </div>
-                <div>
-                    {data.q3_cleaningenddate} to {data.q3_cleaningstartdate}
-                </div>
-                <div>
-                    {data.q4_cleaningenddate} to {data.q4_cleaningstartdate}
+            <div className="px-10 lg:flex lg:flex-col lg:items-center lg:justify-center">
+                <div className="flex flex-col max-w-7xl">
+                    <div className="my-10 text-3xl lg:text-5xl text-center">
+                        Hawker open or not?
+                    </div>
+                    <div className="bg-[#F5F5F5] w-full rounded-[1rem] flex flex-col justify-center items-center py-10 px-6 lg:py-20 lg:px-10">
+                        <img
+                            className="w-20 lg:w-32 mb-6"
+                            src="/images/open.png"
+                        />
+                        <div className="text-2xl lg:text-4xl font-bold text-center mb-2">
+                            {data.name}
+                        </div>
+                        <div className="text-xl lg:text-2xl text-center mb-10">
+                            {data.address_myenv}
+                        </div>
+                        <div className="flex flex-col lg:flex-row justify-center items-center bg-[#FFF4C1] px-10 py-6">
+                            <img
+                                className="w-12 h-12 lg:w-16 lg:h-16 mr-6 mb-4 lg:mb-0"
+                                src="/images/bucket.png"
+                            />
+                            <div className="flex flex-col items-center justify-center">
+                                <div className="text-xl lg:text-2xl text-center">
+                                    Next scheduled cleaning date
+                                </div>
+                                <div className="text-xl lg:text-2xl font-bold text-center">
+                                    7 May 2022, Sat
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <Link
+                        className="my-10 text-center text-[#E50027] text-xl underline"
+                        href="/"
+                    >
+                        Search another hawker centre
+                    </Link>
+                    <div className="mb-4 text-center text-[#a99743]">
+                        Designed and built because we wanted to eat, but hawker
+                        centre was closed for cleaning. Data from
+                        https://data.gov.sg/ ♥
+                    </div>
                 </div>
             </div>
         </>
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -38,79 +38,92 @@ export default function Welcome(props) {
     return (
         <>
             <Head title="Hawkers" />
-            <div className="h-screen px-4 mt-24 lg:mt-0 lg:flex lg:flex-col lg:items-center lg:justify-center">
-                <div className="mb-12">
-                    <FloatingIcons />
-                </div>
-                <div className="mb-4 text-3xl lg:text-5xl text-center">
-                    Hawker open or not?
-                </div>
-                <div className="mb-6 text-xl lg:text-3xl text-center">
-                    Find out when hawker centres are open or closed for cleaning
-                </div>
-                <div className="max-w-3xl mx-auto w-full relative">
-                    <Combobox
-                        value={selectedHawker}
-                        onChange={setSelectedHawker}
-                    >
-                        <div className="absolute inset-y-0 left-0 flex items-center pl-5">
-                            <img className="w-6" src="/images/map-pin.png" />
-                        </div>
-                        <Combobox.Input
-                            className={`w-full h-11 border-none focus:ring-0 shadow-md shadow-[#ffc700] pl-14 ${
-                                query ? "rounded-t-3xl" : "rounded-3xl"
-                            }`}
-                            onChange={(event) => setQuery(event.target.value)}
-                            displayValue={(hawker) => hawker.name}
-                        />
-                        <Transition
-                            as={Fragment}
-                            leave=""
-                            leaveFrom="opacity-100"
-                            leaveTo="opacity-0"
-                            afterLeave={() => setQuery("")}
+            <div className="px-10 lg:flex lg:flex-col lg:items-center lg:justify-center">
+                <div className="flex flex-col max-w-7xl">
+                    <div className="mt-20 mb-6 lg:mt-40 lg:mb-12">
+                        <FloatingIcons />
+                    </div>
+                    <div className="mb-4 text-3xl lg:text-5xl text-center">
+                        Hawker open or not?
+                    </div>
+                    <div className="mb-6 text-xl lg:text-3xl text-center">
+                        Find out when hawker centres are open or closed for
+                        cleaning
+                    </div>
+                    <div className="max-w-3xl mx-auto w-full relative">
+                        <Combobox
+                            value={selectedHawker}
+                            onChange={setSelectedHawker}
                         >
-                            <div className="absolute max-w-3xl w-full bg-white rounded-b-3xl shadow-md shadow-[#ffc700]">
-                                <Combobox.Options>
-                                    {filtered.map((hawker) => (
-                                        <Combobox.Option
-                                            key={hawker._id}
-                                            value={hawker}
-                                            className={({ active }) =>
-                                                `relative cursor-default select-none py-2 pl-14 pr-4 ${
-                                                    active
-                                                        ? "bg-[#f2d860] text-black bg-opacity-25"
-                                                        : "text-black"
-                                                }`
-                                            }
-                                        >
-                                            {/* todo: can this be using named route instead? */}
-                                            <Link
-                                                href={`/hawkers/${hawker.url}`}
-                                            >
-                                                {hawker.name}
-                                            </Link>
-                                        </Combobox.Option>
-                                    ))}
-                                    {query && filtered.length === 0 && (
-                                        <Combobox.Option
-                                            disabled={true}
-                                            value={null}
-                                            className="relative cursor-default select-none py-2 pl-14 pr-4 break-words"
-                                        >
-                                            <div>
-                                                We couldn't find "{query}"
-                                            </div>
-                                            <div className="text-xs text-gray-400">
-                                                Try searching for hawker centre
-                                                name or address
-                                            </div>
-                                        </Combobox.Option>
-                                    )}
-                                </Combobox.Options>
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-5">
+                                <img
+                                    className="w-6"
+                                    src="/images/map-pin.png"
+                                />
                             </div>
-                        </Transition>
-                    </Combobox>
+                            <Combobox.Input
+                                className={`w-full h-11 border-none focus:ring-0 shadow-md shadow-[#ffc700] pl-14 ${
+                                    query ? "rounded-t-3xl" : "rounded-3xl"
+                                }`}
+                                onChange={(event) =>
+                                    setQuery(event.target.value)
+                                }
+                                displayValue={(hawker) => hawker.name}
+                            />
+                            <Transition
+                                as={Fragment}
+                                leave=""
+                                leaveFrom="opacity-100"
+                                leaveTo="opacity-0"
+                                afterLeave={() => setQuery("")}
+                            >
+                                <div className="absolute max-w-3xl w-full bg-white rounded-b-3xl shadow-md shadow-[#ffc700]">
+                                    <Combobox.Options>
+                                        {filtered.map((hawker) => (
+                                            <Combobox.Option
+                                                key={hawker._id}
+                                                value={hawker}
+                                                className={({ active }) =>
+                                                    `relative cursor-default select-none py-2 pl-14 pr-4 ${
+                                                        active
+                                                            ? "bg-[#f2d860] text-black bg-opacity-25"
+                                                            : "text-black"
+                                                    }`
+                                                }
+                                            >
+                                                {/* todo: can this be using named route instead? */}
+                                                <Link
+                                                    href={`/hawkers/${hawker.url}`}
+                                                >
+                                                    {hawker.name}
+                                                </Link>
+                                            </Combobox.Option>
+                                        ))}
+                                        {query && filtered.length === 0 && (
+                                            <Combobox.Option
+                                                disabled={true}
+                                                value={null}
+                                                className="relative cursor-default select-none py-2 pl-14 pr-4 break-words"
+                                            >
+                                                <div>
+                                                    We couldn't find "{query}"
+                                                </div>
+                                                <div className="text-xs text-gray-400">
+                                                    Try searching for hawker
+                                                    centre name or address
+                                                </div>
+                                            </Combobox.Option>
+                                        )}
+                                    </Combobox.Options>
+                                </div>
+                            </Transition>
+                        </Combobox>
+                    </div>
+                    <div className="mt-10 my-4 text-center text-[#a99743]">
+                        Designed and built because we wanted to eat, but hawker
+                        centre was closed for cleaning. Data from
+                        https://data.gov.sg/ ♥
+                    </div>
                 </div>
             </div>
         </>
