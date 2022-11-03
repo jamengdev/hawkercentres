4f2251d231dfb1d5cfbdc2c5077fb964cff75721
Jame Ng <build@jameng.dev>
2022-11-03 17:00:48 +0800

    animate the floating icons

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -136,16 +136,7 @@ export default function Home(props) {
 
 const FloatingIcons = () => {
     return (
-        <div className="flex space-x-12 overflow-hidden">
-            <img className="w-16 lg:w-20" src="/images/ramen-one.png" />
-            <img className="w-16 lg:w-20" src="/images/ramen-two.png" />
-            <img className="w-16 lg:w-20" src="/images/curry-rice-one.png" />
-            <img className="w-16 lg:w-20" src="/images/ramen-one.png" />
-            <img className="w-16 lg:w-20" src="/images/ramen-two.png" />
-            <img className="w-16 lg:w-20" src="/images/curry-rice-one.png" />
-            <img className="w-16 lg:w-20" src="/images/ramen-one.png" />
-            <img className="w-16 lg:w-20" src="/images/ramen-two.png" />
-            <img className="w-16 lg:w-20" src="/images/curry-rice-one.png" />
+        <div id="floating-icons" className="h-20">
         </div>
     );
 };
